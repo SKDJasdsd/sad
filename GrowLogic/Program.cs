31bbd28bbc6cf25c4c4be87eecbcbbef3cbae7d2
SKDJasdsd <sjasdasd@yandex.ru>
2019-10-21 11:30:45 +0500

    Add project files.

---
+++ GrowLogic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace GrowLogic
+{
+    class Program
+    {
+        static void do_something()
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Thread.Sleep(50);
+                do_something();
+            }
+        }
+    }
+}
